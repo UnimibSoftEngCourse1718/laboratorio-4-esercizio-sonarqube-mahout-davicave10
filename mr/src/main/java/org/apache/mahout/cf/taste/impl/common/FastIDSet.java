d1694219f16b314be2900091645e148c38d9b73b
Davide <Davide@192.168.1.112>
2018-01-12 13:08:50 +0100

    Renamed keys variable

--- mr/src/main/java/org/apache/mahout/cf/taste/impl/common/FastIDSet.java
+++ mr/src/main/java/org/apache/mahout/cf/taste/impl/common/FastIDSet.java
@@ -294,8 +294,8 @@ public final class FastIDSet implements Serializable, Cloneable, Iterable<Long> 
   @Override
   public int hashCode() {
     int hash = 0;
-    long[] keys = this.keys;
-    for (long key : keys) {
+    long[] keysID = this.keysID;
+    for (long key : keysID) {
       if (key != NULL && key != REMOVED) {
         hash = 31 * hash + ((int) (key >> 32) ^ (int) key);
       }
